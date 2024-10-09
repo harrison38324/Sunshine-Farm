ba1c4eba6b253bbbcba77c9deecfd5ac8d93eaaf
Harrison Li <harrison38324@gmail.com>
2024-10-09 13:15:24 -0700

    Added Specifications for PlantsSlots

---
+++ src/main/model/PlantsSlots.java
@@ -0,0 +1,17 @@
+package model;
+
+public class PlantsSlots extends PlantsStorage {
+
+    // EFFECTS: the slots for players to plant
+    //          the Plants they bought
+    public PlantsSlots(){
+        super();
+    }
+
+    // REQUIRES: num >= 0
+    // EFFECTS: get the "num th" Plants in the PlantsSlots
+    public void getPlants(int num){
+        //stub
+    }
+
+}
