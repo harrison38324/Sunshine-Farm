46e72700685c59d548d2834966b1599ad3ec7fa5
Harrison Li <harrison38324@gmail.com>
2024-10-08 21:57:50 -0700

    Added Specification for Plants

---
+++ src/main/model/Plants.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Plants {
+    private String plantsName;
+    private int price;
+
+    // REQUIRES: price>=0 plantsName cannot be an empty string
+    // EFFECTS: set a new kind of plants by assigned the plantsName and its price
+    public Plants(String plantsName,int price){
+        //stub
+    }
+
+    public int getPrice(){
+        //stub
+    }
+     
+    public int getPlantsName(){
+        //stub
+    }
+}
+
