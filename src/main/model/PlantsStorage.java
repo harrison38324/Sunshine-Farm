c661e98ecf4a88dc836e3d45b75b63d72813088b
Harrison Li <harrison38324@gmail.com>
2024-10-09 13:01:39 -0700

    Added Specifications for PlantsStorage

---
+++ src/main/model/PlantsStorage.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.List;
+
+public class PlantsStorage {
+    private List<Plants> plantsStorage;
+
+    public PlantsStorage(){
+        //stub
+    } 
+
+    // MODIFIES: this
+    // EFFECTS: add a Plants into plantsStorage
+    public void addPlants(Plants plants){
+
+    }
+
+    // REQUIRES: num>=0
+    // MODIFIES: this
+    // EFFECTS: remove a Plants from the Storage
+    public void remove(int num){
+        //stub
+    }
+
+    // EFFECTS: generate a Namelist of the Plants
+    //          in the storage in original order
+    public List<String> getNameList(){
+        //stub
+    }
+
+    public List<Plants> getPlantsStorage(){
+        //stub
+    }
+}
