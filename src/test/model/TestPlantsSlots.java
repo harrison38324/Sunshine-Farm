584da8305575e4a76cf1ce407e83b6daf43f37b3
Harrison Li <harrison38324@gmail.com>
2024-10-10 15:11:42 -0700

    Fixed bugs for TestPlantsSlots

--- src/test/model/TestPlantsSlots.java
+++ src/test/model/TestPlantsSlots.java
@@ -2,23 +2,40 @@ package model;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-public class TestPlantsSlots extends TestPlantsStorage {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestPlantsSlots {
     private PlantsSlots plantsSlots;
+    private Plants norPlants;
+    private Plants rarePlants;
+    private Plants legendPlants;
+    private List<Plants> plantsList;
 
     @BeforeEach
     void runBefore(){
         plantsSlots = new PlantsSlots();
+        norPlants = new Plants("Apple tree", 10, 100);
+        rarePlants = new Plants("Blueberry", 100, 200);
+        legendPlants = new Plants("gold tree", 1000, 500);
 
         plantsSlots.addPlants(norPlants);
         plantsSlots.addPlants(rarePlants);
         plantsSlots.addPlants(legendPlants);
+
+        plantsList = new ArrayList<>();
+        plantsList.add(norPlants);
+        plantsList.add(rarePlants);
+        plantsList.add(legendPlants);
     }
 
     @Test
     void testGetPlants(){
-        assertEquals(plantsSlots.getPlantsStorage(),plantsList2);
+        assertEquals(plantsSlots.getPlants(0),norPlants);
+        assertEquals(plantsSlots.getPlants(1),rarePlants);
+        assertEquals(plantsSlots.getPlants(2),legendPlants);
     }
 }
