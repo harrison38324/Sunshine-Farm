7cc23dfaf6de0e29a6b24d4e836e6fa2838a2e8e
Harrison Li <harrison38324@gmail.com>
2024-10-10 13:48:10 -0700

    Added Implementations for PlantsStorage

--- src/main/model/PlantsStorage.java
+++ src/main/model/PlantsStorage.java
@@ -1,35 +1,41 @@
 package model;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class PlantsStorage {
     private List<Plants> plantsStorage;
+    private List<String> plantsStorageNameList;
 
     //EFFECTS: generate a list which stores Plants
     public PlantsStorage(){
-        //stub
+        plantsStorage = new ArrayList<>();
+        plantsStorageNameList = new ArrayList<>();
     } 
 
     // MODIFIES: this
     // EFFECTS: add a Plants into plantsStorage
     public void addPlants(Plants plants){
-
+        plantsStorage.add(plants);
     }
 
     // REQUIRES: num>=0
     // MODIFIES: this
     // EFFECTS: remove a Plants from the Storage
     public void remove(int num){
-        //stub
+        plantsStorage.remove(num);
     }
 
     // EFFECTS: generate a Namelist of the Plants
     //          in the storage in original order
     public List<String> getNameList(){
-        //stub
+        for(int i = 0; i < plantsStorage.size();i++){
+            plantsStorageNameList.add(plantsStorage.get(i).getPlantsName());
+        }
+        return plantsStorageNameList;
     }
 
     public List<Plants> getPlantsStorage(){
-        //stub
+        return plantsStorage;
     }
 }
