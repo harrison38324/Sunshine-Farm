08ee1668041e7212485377f29b51108c9087556a
Harrison Li <harrison38324@gmail.com>
2024-10-10 14:44:58 -0700

    Fixed bug for PlantsStorage

--- src/main/model/PlantsStorage.java
+++ src/main/model/PlantsStorage.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class PlantsStorage {
-    private List<Plants> plantsStorage;
+    protected List<Plants> plantsStorage;
     private List<String> plantsStorageNameList;
 
     //EFFECTS: generate a list which stores Plants
