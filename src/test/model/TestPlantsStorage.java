dbbeb9dad209453a7fa38d2c75e27f66bebc3b4a
Harrison Li <harrison38324@gmail.com>
2024-10-10 13:48:41 -0700

    Fixed bug for TestPlantsStorage

--- src/test/model/TestPlantsStorage.java
+++ src/test/model/TestPlantsStorage.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TestPlantsStorage {
