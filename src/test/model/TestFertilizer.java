1b174a295e5b15d972f74c2d4d7c1353b19b08cd
Harrison Li <harrison38324@gmail.com>
2024-10-09 22:15:26 -0700

    Added TestFertilizer

---
+++ src/test/model/TestFertilizer.java
@@ -0,0 +1,47 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestFertilizer {
+    private Fertilizer fertilizer;
+
+    @BeforeEach
+    void runBefore(){
+        fertilizer = new Fertilizer("Normal Fertilizer", 10, 1);
+    }
+
+    @Test
+    void testGetPrice(){
+        assertEquals(fertilizer.getPrice(), 10);  
+    }
+
+    @Test
+    void testGetName(){
+        assertEquals(fertilizer.getName(), "Normal Fertilizer");
+    }
+
+    @Test
+    void testGetTimeReduced(){
+        assertEquals(fertilizer.getTimeReduced(), 1);
+    }
+
+    @Test
+    void testIncreasePrice(){
+        fertilizer.increasePrice(10);
+        assertEquals(fertilizer.getPrice(), 20);
+        fertilizer.increasePrice(2);
+        assertEquals(fertilizer.getPrice(), 22);
+    }
+
+    @Test
+    void testDecreasePrice(){
+        fertilizer.decreasePrice(2);
+        assertEquals(fertilizer.getPrice(), 8);
+        fertilizer.decreasePrice(5);
+        assertEquals(fertilizer.getPrice(), 3);
+    }
+}
