095683378ac10f0f7b71ec6ab260127a0785c792
Harrison Li <harrison38324@gmail.com>
2024-10-21 00:21:06 -0700

    Fertilizer extends AgricultureEntity

--- src/main/model/Fertilizer.java
+++ src/main/model/Fertilizer.java
@@ -1,43 +1,41 @@
 package model;
 
-public class Fertilizer {
-    private String name;
-    private int price;
-    private int timeReduced;
+public class Fertilizer extends AgriculturalEntity{
+    // private String name;
+    // private int price;
+    // private int timeReduced;
 
     // REQUIRES: price >= 0 ,TimeReduced >= 0, Name cannot be empty String
     // EFFECTS: set a new kind of Fertilizer by assigned the Name
     // its price and the time it reduces the plant to grow up
     public Fertilizer(String name, int price, int timeReduced) {
-        this.name = name;
-        this.price = price;
-        this.timeReduced = timeReduced;
+        super(name, price, timeReduced);
     }
 
-    public int getPrice() {
-        return price;
-    }
+    // public int getPrice() {
+    //     return price;
+    // }
 
-    public String getName() {
-        return name;
-    }
+    // public String getName() {
+    //     return name;
+    // }
 
-    public int getTimeReduced() {
-        return timeReduced;
-    }
+    // public int getTimeReduced() {
+    //     return timeReduced;
+    // }
 
-    // REQUIRES: money >= 0
-    // MODIFIES: this
-    // EFFECTS: increase price by the input money
-    public void increasePrice(int money) {
-        price += money;
-    }
+    // // REQUIRES: money >= 0
+    // // MODIFIES: this
+    // // EFFECTS: increase price by the input money
+    // public void increasePrice(int money) {
+    //     price += money;
+    // }
 
-    // REQUIRES: money >= 0, money<=Price
-    // MODIFIES: this
-    // EFFECTS: decrease price by the input money
-    public void decreasePrice(int money) {
-        price -= money;
-    }
+    // // REQUIRES: money >= 0, money<=Price
+    // // MODIFIES: this
+    // // EFFECTS: decrease price by the input money
+    // public void decreasePrice(int money) {
+    //     price -= money;
+    // }
 
 }
