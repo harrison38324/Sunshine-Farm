a8de1074813d0c3d1bd34183c327fe0837e2e3f3
Harrison Li <harrison38324@gmail.com>
2024-10-21 00:21:19 -0700

    Plants extends AgricultureEntity

--- src/main/model/Plants.java
+++ src/main/model/Plants.java
@@ -1,60 +1,58 @@
 package model;
 
-public class Plants {
-    private String plantsName;
-    private int price;
-    private int growthTime;
+public class Plants extends AgriculturalEntity{
+    // private String plantsName;
+    // private int price;
+    // private int growthTime;
 
     // REQUIRES: price>=0 , growthTime>=0 plantsName cannot be an empty string
     // EFFECTS: set a new kind of plants by assigned the plantsName
     // its price and the time it consumed to grow up
     public Plants(String plantsName, int price, int growthTime) {
-        this.plantsName = plantsName;
-        this.price = price;
-        this.growthTime = growthTime;
+        super(plantsName,price,growthTime);
     }
 
-    public int getPrice() {
-        return price;
-    }
+    // public int getPrice() {
+    //     return price;
+    // }
 
-    public String getPlantsName() {
-        return plantsName;
-    }
+    // public String getPlantsName() {
+    //     return plantsName;
+    // }
 
-    public int getGrowthTime() {
-        return growthTime;
-    }
+    // public int getGrowthTime() {
+    //     return growthTime;
+    // }
 
-    // REQUIRES: money >= 0
-    // MODIFIES: this
-    // EFFECTS: increase the price of the plant by the input money
-    public void increasePrice(int money) {
-        price += money;
-    }
+    // // REQUIRES: money >= 0
+    // // MODIFIES: this
+    // // EFFECTS: increase the price of the plant by the input money
+    // public void increasePrice(int money) {
+    //     price += money;
+    // }
 
-    // REQUIRES: money >= 0
-    // MODIFIES: this
-    // EFFECTS: decrease the price of the plant by the input money
-    public void decreasePrice(int money) {
-        price -= money;
-    }
+    // // REQUIRES: money >= 0
+    // // MODIFIES: this
+    // // EFFECTS: decrease the price of the plant by the input money
+    // public void decreasePrice(int money) {
+    //     price -= money;
+    // }
 
-    // REQUIRES: time >= growthTime
-    // MODIFIES: this
-    // EFFECTS: decrease the growthTime by input time
-    public void decreaseGrowthTime(int time) {
-        growthTime -= time;
-    }
+    // // REQUIRES: time >= growthTime
+    // // MODIFIES: this
+    // // EFFECTS: decrease the growthTime by input time
+    // public void decreaseGrowthTime(int time) {
+    //     growthTime -= time;
+    // }
 
     // MODIFIES: this
     // EFFECTS: apply the fertilizer to the plants
     public void useFertilizer(Fertilizer fertilizer) {
-        decreaseGrowthTime(fertilizer.getTimeReduced());
+        decreaseTime(fertilizer.getTime());
     }
 
-    // REQUIRES: time >=0
-    public void setGrowthTime(int time) {
-        this.growthTime = time;
-    }
+    // // REQUIRES: time >=0
+    // public void setGrowthTime(int time) {
+    //     this.growthTime = time;
+    // }
 }
