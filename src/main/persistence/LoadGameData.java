201f3da578ad86df61cdf0a1150a53c38e716938
Harrison Li <harrison38324@gmail.com>
2024-10-23 14:01:27 -0700

    Added Specifications for LoadGameData

---
+++ src/main/persistence/LoadGameData.java
@@ -0,0 +1,15 @@
+package persistence;
+
+public class LoadGameData {
+    private String source;
+
+    public LoadGameData(String source) {
+        this.source = source;
+    }
+
+    // EFFECTS: load the game
+    // data(plantsStorage,fertilizerStorage,wallet,plantsSlot)
+    public void loadGameData(String source) {
+
+    }
+}
