90de63ed1c9c74bae4881091821e78748a19425a
Harrison Li <harrison38324@gmail.com>
2024-10-23 22:34:34 -0700

    Added Implementations for LoadGameData

--- src/main/persistence/LoadGameData.java
+++ src/main/persistence/LoadGameData.java
@@ -2,6 +2,13 @@ package persistence;
 
 import java.io.IOException;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import model.AgriculturalEntity;
+import model.AgriculturalEntityStorage;
 import model.FertilizerStorage;
 import model.PlantsSlots;
 import model.PlantsStorage;
@@ -10,15 +17,39 @@ import model.Wallet;
 // the class used for Game Data loading
 public class LoadGameData {
     private String source;
+    private JSONObject gameData;
 
     public LoadGameData(String source) {
         this.source = source;
     }
 
+    // MODIFIES: this
+    // EFFECTS: load AgriculturalEntityStorage Data
+    private void loadAgriculturalEntityStorageData(AgriculturalEntityStorage agriculturalEntityStorage,String storageName,String timeName){
+        JSONArray agriculturalEntityStorageArray = gameData.getJSONArray(storageName);
+        agriculturalEntityStorage.clear();
+        for(int i = 0 ; i < agriculturalEntityStorageArray.length();i++){
+            JSONObject newAgriculturalEntity = agriculturalEntityStorageArray.getJSONObject(i);
+            String name = newAgriculturalEntity.getString("name");
+            int price = newAgriculturalEntity.getInt("price");
+            int time = newAgriculturalEntity.getInt(timeName);
+            AgriculturalEntity gotAgriculturalEntity = new AgriculturalEntity(name, price, time);
+            agriculturalEntityStorage.add(gotAgriculturalEntity);
+        }
+    }
+
+    // MODIFIES: this
     // EFFECTS: load the game
     // data(plantsStorage,fertilizerStorage,wallet,plantsSlot)
     public void loadGameData(Wallet wallet, FertilizerStorage fertilizerStorage, PlantsStorage plantsStorage,
             PlantsSlots plantsSlot) throws IOException {
-        // stub
+        String content = new String(Files.readAllBytes(Paths.get(source)));
+        gameData = new JSONObject(content);
+
+        int moneyData = gameData.getInt("money");
+        wallet.setBalance(moneyData);
+        loadAgriculturalEntityStorageData(fertilizerStorage,"fertilizerStorage","timeReduced");
+        loadAgriculturalEntityStorageData(plantsStorage,"plantsStorage","growthTime");
+        loadAgriculturalEntityStorageData(plantsSlot,"plantsSlot","growthTime");
     }
 }
