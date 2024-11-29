ef95fec615cc85a0357061f828d0671fdc7cca8b
Harrison Li <harrison38324@gmail.com>
2024-11-28 16:36:07 -0800

    Deleted useless field in PlantsStorage

--- src/main/model/PlantsStorage.java
+++ src/main/model/PlantsStorage.java
@@ -1,12 +1,7 @@
 package model;
 
-import java.util.List;
-// import java.util.ArrayList;
-
 // a list of Plants, contains the Plants you bought but haven't planted
 public class PlantsStorage extends AgriculturalEntityStorage {
-    protected List<Plants> plantsStorage;
-    // private List<String> plantsStorageNameList;
 
     // EFFECTS: generate a list which stores Plants
     public PlantsStorage() {
